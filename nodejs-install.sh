868968bce413f97e7962513a523762f9c2bc5c49
czj267 <czj267@163.com>
2018-07-02 21:15:23 +0800

    Create nodejs-install.sh

---
+++ nodejs-install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+src_dir="/root/src"
+if [ ! -d ${src_dir} ]; then
+  mkdir ${src_dir} -p
+fi
+cd ${src_dir}
+if [ ! -f "${src_dir}/node-v8.11.3-linux-x64.tar.xz" ]; then
+  wget 'https://nodejs.org/dist/v8.11.3/node-v8.11.3-linux-x64.tar.xz'
+fi
+xz -d 'node-v8.11.3-linux-x64.tar.xz'
+tar -xvf 'node-v8.11.3-linux-x64.tar'
+
+mv node-v8.11.3-linux-x64 node-8.11
+
+mv node-8.11 /usr/local/
