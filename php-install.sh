fcde058c50632ec41759847a869695b392095ee7
czj267 <czj267@163.com>
2018-07-14 01:06:19 +0800

    Update php-install.sh

--- php-install.sh
+++ php-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # 安装php
 # 安装依赖
-apt install -y autoconf libmcrypt-dev libcurl4-openssl-dev pkg-config libjpeg-dev libpng-dev libxml2-dev libedit-dev
+apt install -y autoconf libmcrypt-dev libcurl4-openssl-dev pkg-config libjpeg-dev libpng-dev libxml2-dev libedit-dev libssl-dev
 add-apt-repository ppa:no1wantdthisname/ppa
 apt update
 apt install libfreetype6 libfreetype6-dev -y
